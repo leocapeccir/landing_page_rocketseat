8d1050f009bd0cd828516dc79f605887fc715099
Leonardo Ramos <103533833+leocapeccir@users.noreply.github.com>
2024-10-23 09:20:09 -0300

    chore: last changes

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 function toggleMode() {
-    const html = document.documentElement
-    html.classList.toggle('light')
+  const html = document.documentElement;
+  html.classList.toggle("light");
 
-    const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img");
 
-    if(html.classList.contains('light')) {
-        img.setAttribute('src','./assets/avatar.png')
-        document.querySelector('p').innerText = '@leocapeccir'
-
-    } else {
-        img.setAttribute('src','./assets/avatar1.png')
-        document.querySelector('p').innerText = '@maikybrito'
-
-    }
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar.png");
+  } else {
+    img.setAttribute("src", "./assets/avatar1.png");
+  }
 }
-
